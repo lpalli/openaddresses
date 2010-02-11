14fa74c3af0f6b4bd2c0cd03c6c37b3f1f4cfee2
Cédric Moullet <cedric.moullet@gmail.com>
2010-02-11 22:32:46 +0000

    Add feature check during editing

--- openaddresses/openaddresses/public/app/js/OpenAddressesEditControl.js
+++ openaddresses/openaddresses/public/app/js/OpenAddressesEditControl.js
@@ -6,6 +6,16 @@
  * of the license.
  */
 
+/*
+ * @requires OpenLayers/Control.js
+ * @include OpenLayers/Handler/Click.js
+ * @include OpenLayers/Projection.js
+ * @include OpenLayers/Control/ModifyFeature.js
+ * @include OpenLayers/BaseTypes/LonLat.js
+ * @include OpenLayers/Feature/Vector.js
+ * @include OpenLayers/Geometry/Point.js
+ */
+
 Ext.namespace("openaddresses");
 
 openaddresses.EditControl = OpenLayers.Class(OpenLayers.Control, {
@@ -46,21 +56,46 @@ openaddresses.EditControl = OpenLayers.Class(OpenLayers.Control, {
         //     - show a movable circle in the map at the digitized poistion
         //     - show the edition popup
         var clickedPosition = openaddresses.layout.map.getLonLatFromViewPortPx(evt.xy);
+        var clickePositionWGS84 = clickedPosition.clone();
+        clickePositionWGS84.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
 
         var vectorLayer = openaddresses.layout.map.getLayersByName('DrawingLayer')[0];
 
-        // 1. 
-        var feature = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(clickedPosition.lon, clickedPosition.lat));
+        // 1. Check if an address exists at this position
+        Ext.Ajax.request({
+            url: 'addresses/',
+            method: 'GET',
+            success: function(responseObject) {
+                var mapfishFeatures = eval('(' + responseObject.responseText + ')');
 
-        if (!openaddresses.layout.map.modifyFeatureControl) {
-            openaddresses.layout.map.modifyFeatureControl = new OpenLayers.Control.ModifyFeature(vectorLayer);
-            openaddresses.layout.map.addControl(openaddresses.layout.map.modifyFeatureControl);
-            openaddresses.layout.map.modifyFeatureControl.activate();
-        }
+                if (!openaddresses.layout.map.modifyFeatureControl) {
+                    openaddresses.layout.map.modifyFeatureControl = new OpenLayers.Control.ModifyFeature(vectorLayer);
+                    openaddresses.layout.map.addControl(openaddresses.layout.map.modifyFeatureControl);
+                    openaddresses.layout.map.modifyFeatureControl.activate();
+                }
+                var feature;
+                if (mapfishFeatures.features.length === 0) {
+                    feature = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(clickedPosition.lon, clickedPosition.lat));
+                } else {
+                    var featurePosition = new OpenLayers.LonLat(mapfishFeatures.features[0].geometry.coordinates[0], mapfishFeatures.features[0].geometry.coordinates[1]);
+                    featurePosition.transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+                    feature = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(featurePosition.lon, featurePosition.lat));
+                }
 
-        vectorLayer.addFeatures(feature);
-        openaddresses.layout.map.modifyFeatureControl.selectControl.select(feature);
-        openaddresses.layout.map.modifyFeatureControl.selectControl.handlers.feature.feature = feature;
+                vectorLayer.addFeatures(feature);
+                openaddresses.layout.map.modifyFeatureControl.selectControl.select(feature);
+                openaddresses.layout.map.modifyFeatureControl.selectControl.handlers.feature.feature = feature;
+            },
+            failure: function() {
+                alert('Error in addresses GET query');
+            },
+            params: { lon: clickePositionWGS84.lon,
+                lat: clickePositionWGS84.lat,
+                //tolerance: 0.0001
+                tolerance: 3
+            }
+        });
+
     },
 
     /** private: method[onDblclick]
