75d919373c3f8cb9e0c30df37de21555815403d9
Cédric Moullet <cedric.moullet@gmail.com>
2010-02-13 08:30:02 +0000

    Add session manager for complete editing workflow

---
+++ openaddresses/openaddresses/controllers/sessionManager.py
@@ -0,0 +1,24 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from openaddresses.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class SessionmanagerController(BaseController):
+
+    def index(self):
+        return 'Hello World'
+
+    def checkSession(self):
+        for key in session:
+           if key == 'authenticated':
+              return 'True'
+        return 'False'
+
+    def createSession(self):
+        session['authenticated'] = 'True'
+        session.save()
+        return session['authenticated']
