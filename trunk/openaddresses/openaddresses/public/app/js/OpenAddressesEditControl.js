a2e106e52af8aaa3b8ce91fce2c2d58cf6fd8348
cedric.moullet <cedric.moullet@9b686bd0-0aac-11df-b730-f17d7b42d8b9>
2010-03-12 19:26:04 +0000

    Update license

--- trunk/openaddresses/openaddresses/public/app/js/OpenAddressesEditControl.js
+++ trunk/openaddresses/openaddresses/public/app/js/OpenAddressesEditControl.js
@@ -1,9 +1,20 @@
-/**
- * Copyright (c) 2008-2009 The Open Source Geospatial Foundation
+/*
+ * Copyright (C) 2009  www.openaddresses.org
  *
- * Published under the BSD license.
- * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
- * of the license.
+ * This file is part openaddresses.org
+ *
+ * www.openaddresses.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * www.openaddresses.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with www.openaddresses.org.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
