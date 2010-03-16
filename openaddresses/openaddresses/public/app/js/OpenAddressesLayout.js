6ec7fda302b10017bbcb76f6662eac22fa506771
Cédric Moullet <cedric.moullet@gmail.com>
2010-03-16 05:30:37 +0000

    Never commit something untested !!!

--- openaddresses/openaddresses/public/app/js/OpenAddressesLayout.js
+++ openaddresses/openaddresses/public/app/js/OpenAddressesLayout.js
@@ -538,7 +538,7 @@ openaddresses.layout = (function() {
 
         hideWaitingMask: function() {
             if (Ext.get('waiting')) {
-                Ext.get('waiting').style.display = "none";
+                Ext.get('waiting').dom.style.display = "none";
             }
         },
 
