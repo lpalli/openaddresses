f972e4041fcf3ea89b46e11cf1dc4b7caa3df63f
cedric.moullet <cedric.moullet@9b686bd0-0aac-11df-b730-f17d7b42d8b9>
2010-03-29 19:21:55 +0000

    Add table export (for prod server)

--- trunk/openaddresses/data/dailyDatabaseBackup.sh
+++ trunk/openaddresses/data/dailyDatabaseBackup.sh
@@ -2,4 +2,5 @@
 BACKUP_DIR="/home/admin/backup"
 PGUSER="postgres"
 time=`date '+%d'-'%m'`
-/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER openaddresses | gzip > $BACKUP_DIR/backup-$time.gz 
+/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5433 openaddresses | gzip > $BACKUP_DIR/backup-$time.gz 
+/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5433 -t address openaddresses | gzip > $BACKUP_DIR/backup-address_$time.gz
