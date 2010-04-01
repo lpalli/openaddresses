40a49a8ce0ba385465666bc02e3e4137069edce0
cedric.moullet <cedric.moullet@9b686bd0-0aac-11df-b730-f17d7b42d8b9>
2010-04-01 20:00:19 +0000

    Add luxemburgish (with mk, since this language .. doesn't exist...)

---
+++ trunk/openaddresses/openaddresses/public/app/lang/mk.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2009  www.openaddresses.org
+ *
+ * This file is part openaddresses.org
+ *
+ * www.openaddresses.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * www.openaddresses.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with www.openaddresses.org.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+OpenLayers.Lang["mk"] = OpenLayers.Util.applyDefaults({
+    '...still loading layers…':'… d\'Donnéen gi gelueden…',
+    'About':'Iwwer OpenAddresses',
+    'Address':'Adress',
+    'Address Editor':'Adresseneditor',
+    'Address Validation':'Adressvalidéirung',
+    'Addresses':'Adressen',
+    'Cancel':'Ofbriechen',
+    'City':'Staad',
+    'Click in the map':'Klick an d\'Kaart',
+    'Country':'Land',
+    'Create':'Erstellen',
+    'Delete':'Läschen',
+    'Deletion Confirmation':'Läschen confirméiren',
+    'Digitized':'Digitaliséiert',
+    'Digitized Position':'Digitaliséiert Positioun',
+    'Do you really want to delete this address ?':'Wëllt Dir des Adress wierklech läschen',
+    'Documentation of GeoCoding and Reverse GeoCoding services':'Beschreiwung von Geokodéirungs- an Reverse Geokodéierungsservicer',
+    'Download':'Rooflueden',
+    'Download addresses':'Adressdatei rooflueden',
+    'DrawingLayer':'Zeechelayer',
+    'Empty Layer':'Eidele Layer',
+    'Error during data deletion':'Fehler während des Läschen',
+    'Error during data storage':'Fehler während dem Speicheren',
+    'Fill the address attributes':'Fëllt d\'Attributer vun der Adress aus',
+    'Fill the empty attributes of this address with the value of the previous edited address. For all attributes except house number and house name.':'Fëllt di eidel Attributer vun deser Adress matt dem Wert vun der virausgegangener Adress. Fier all d\'Attributer ausser der Hausnummer.',
+    'Get missing values from previous address':'Werter vun der virausgegangener Adress iwwerhuelen',
+    'GPS':'GPS',
+    'House name':'Hausnumm',
+    'House number':'Hausnummer',
+    'License':'Lizenz',
+    'Linear interpolation':'Linear interpoléiert',
+    'Ongoing development...<br> About 4.3 millions in OpenAddresses.org.For now, take contact with us through openaddresses[at]googlegroups.com.':'Momentan an der Entwëcklung <br>Ongeféier 5 Milliounen Adressen an OpenAddresses.org. Huelt matt eis Kontakt op enner info[at]openaddresses.org',
+    'Ongoing development...<br> For now, take contact with us through openaddresses[at]googlegroups.com.':'Momentan an der Entwëcklung <br> Huelt matt eis Kontakt op enner info[at]openaddresses.org',
+    'OpenAddresses':'OpenAddresses',
+    'OpenAddresses Code Licence':'OpenAddresses Code-Lizenz',
+    'OpenAddresses Data Licence':'OpenAddresses Date-Lizenz',
+    'OpenAddresses is a web portail for the management of Open Source worldwide localized postal addresses.':'Openaddresses ass ee Portal fier d\'Erhiewung an d\'Prozesséirung vun Hausadressen weltweit.',
+    'Overlay opacity':'Iwwerlagerungstransparenz',
+    'Permalink':'Permalink',
+    'Please confirm that you agree with the OpenAddresses.org license and terms of services':'Bestätegt wannechglifft, datt dir matt der Lizenz vun OpenAddresses.org an den Notzungsbedingungen vun den Dëngschter averstaan sidd.',
+    'Please fill all mandatory fields: ':'Bitte füllen Sie alle notwendigen Felder aus',
+    'Postal code':'Postleitzahl',
+    'Powered by MapFish: the web mapping development framework !':'Powered by MapFish: the web mapping development framework !',
+    'Project Discussion':'Diksussiounsforum',
+    'Project Wiki':'Projetswiki',
+    'Quality':'Qualitéit',
+    'Region':'Regioun',
+    'Save':'Speicheren',
+    'Save the address':'Adress speicheren',
+    'Search in Geonames…':'Sich am Geonames…',
+    'Search location in Geonames':'Uertschaft am Geonames auswielen',
+    'Select a country…':'Land auswielen …',
+    'Select a language…':'Sprooch auswielen …',
+    'Select a quality…':'Qualitéit bestëmmen',
+    'Services':'Dëngschter',
+    'Several companies:':'Verschidde Firmen: campToCamp, INSER, ubiThere',
+    'Several universities:':'Verschidden Unien: University of Applied Sciences Northwestern Switzerland (FHNW) and Carinthia University of Applied Sciences (CUAS)',
+    'Spherical Mercator':'Mercator Sphäresch',
+    'Statistics':'Statistik',
+    'Street':'Strooss',
+    'Thanks for your contribution !':'Merci fier äere Beitrag',
+    'The addresses must be associated to the url of http://www.openaddresses.org.':'D\'Adressen mussen matt der URL \'http://www.openaddresses.org\' markéiert ginn',
+    'The city.':'Uertschaft',
+    'The country of the city.':'Land vun der Uertschaft',
+    'The house name if one exists.':'Hausnumm falls en existéiert',
+    'The house number, if one exists.':'Hausnummer falls se existéiert',
+    'The person or entity that submitted the data is stored in a field CREATED_BY.':'Den Numm oder den Alias vun der Persoun oder der Organisatioun, déi d\'Daten zur Verfügung stellt, gëtt am Attribut \'CREATED_BY\' gespeichert',
+    'The person or entity who created the address. ONLY used for statistic purpose.':'Numm vun der Persoun oder Organisatioun, déi d\'Adress erstallt huet. Des Informatioun gëtt just fier Statistiken benotzt.',
+    'The postal code.':'Postleitzahl',
+    'The quality. If you use the web interface, this will be "Digitized".':'Qualitéit. Falls Dir d\'Adressen iwwer d\'Kaart digitaliséiert an erfaasst, ass de Wäert \'Digitized\'.',
+    'The region. This can be a department, a state, a canton.':'Regioun oder Gebidd oder administrativ Eenheet',
+    'The street name used by the persons living in this area.':'Stroossennumm, wi en offiziell benotzt gëtt',
+    'This project is a joint effort of several persons:':'Dëse Projet ass eng gemeinsam Aarbecht vun verschiddene Persounen: Stefi, Hans-Jörg, Cédric, Jeff, Jean-Luc, Luca, Lukas, Lars, Yves J., Yves',
+    'Upload':'Ropplueden',
+    'User Validation':'Benotzervalidéirung',
+    'Username':'Benotzernumm',
+    'Welcome in OpenAddresses':'Wëllkomm bei Openaddresses',
+    'WGS84':'WGS84',
+    'Yahoo Satellite':'Yahoo Satellitebild',
+    'You are using Chrome. Some unsolved known issues affect this browser. We recommend to use Firefox, Safari or Internet Explorer.':'Dir benotzt Chrome. Leider trieden bei deem Browser momentan e puer kleng Problemer op. Mier empfehlen iech, Firefox oder Safari oder Internet Explorer ze benotzen.',
+    'You are using Internet Explorer 6. We strongly recommend that you update it to a safer and newer version !':'Dir benotzt Internet Explorer 6. Mir empfehlen dringend, Iech eng méi nei an domatt mi sécher Versioun zouzeleeën.'    
+
+});
