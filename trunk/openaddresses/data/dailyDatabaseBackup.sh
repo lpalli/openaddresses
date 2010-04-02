b9f0c9a7409be91bc64ea40bc706ee827e38c4d6
cedric.moullet <cedric.moullet@9b686bd0-0aac-11df-b730-f17d7b42d8b9>
2010-04-02 11:33:58 +0000

    Fix port

--- trunk/openaddresses/data/dailyDatabaseBackup.sh
+++ trunk/openaddresses/data/dailyDatabaseBackup.sh
@@ -2,5 +2,5 @@
 BACKUP_DIR="/home/admin/backup"
 PGUSER="postgres"
 time=`date '+%d'-'%m'`
-/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5433 openaddresses | gzip > $BACKUP_DIR/backup-$time.gz 
-/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5433 -t address openaddresses | gzip > $BACKUP_DIR/backup-address_$time.gz
+/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5432 openaddresses | gzip > $BACKUP_DIR/backup-$time.gz 
+/usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5432 -t address openaddresses | gzip > $BACKUP_DIR/backup-address_$time.gz
