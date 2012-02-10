06b3090f1c406c10937e519bee36f841784670da
cedric.moullet <cedric.moullet@9b686bd0-0aac-11df-b730-f17d7b42d8b9>
2012-02-10 05:18:04 +0000

    Add year for the backup

--- trunk/openaddresses/data/dailyDatabaseBackup.sh
+++ trunk/openaddresses/data/dailyDatabaseBackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 BACKUP_DIR="/home/admin/backup"
 PGUSER="postgres"
-time=`date '+%d'-'%m'`
+time=`date '+%y'-'%m'-'%d'`
 /usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5432 openaddresses | gzip > $BACKUP_DIR/backup-$time.gz 
 /usr/lib/postgresql/8.4/bin/pg_dump -U $PGUSER -p 5432 -t address openaddresses | gzip > $BACKUP_DIR/backup-address_$time.gz
